41d769330eb723a7b7a9108748b41fb9eb73f374
zokjebi <myimgman@gmail.com>
2026-08-11 06:34:07 +0900

    zokjebi.github.io 에 조회되도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,5 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
-  base: "/",
+  base: "/marriage/",
 });
