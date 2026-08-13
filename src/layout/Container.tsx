770f18b0cb21d1e51dc3912f40d580f480e11825
zokjebi <myimgman@gmail.com>
2026-08-13 22:30:11 +0900

    모바일에서 보이는 이미지 보정 (오른쪽 여백 중가)

--- src/layout/Container.tsx
+++ src/layout/Container.tsx
@@ -8,6 +8,10 @@ const Container = styled.div`
   background-color: #ffffff;
   width: 85vw;
   margin: 0 auto;
+
+  @media screen and (max-width: 400px) {
+    width: calc(100vw - 60px);
+  }
   
   @media screen and (min-width: 500px) {
       width: 500px;
